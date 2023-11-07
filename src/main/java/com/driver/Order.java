192131d08e471f98edb244ea46bf80182ef71fc1
siva sankar <sivas2654@gmail.com>
2023-11-07 09:11:13 +0530

    second

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -2,142 +2,29 @@ package com.driver;
 
 public class Order {
 
-
-
     private String id;
     private int deliveryTime;
 
     public Order(String id, String deliveryTime) {
-
-        // The deliveryTime has to converted from string to int and then stored in the attribute
-        //deliveryTime  = HH*60 + MM
-
-        String[] parts = deliveryTime.split(":");
-        int time=0;
-        time+=Integer.parseInt(parts[0])*60;
-        time+=Integer.parseInt(parts[1]);
-        this.id=id;
-        this.deliveryTime=time;
-    }
-    public void setId(String id) {
         this.id = id;
+
+        // The deliveryTime has to converted from string to int and then stored in the attribute deliveryTime  = HH*60 + MM
+//        this.deliveryTime = Integer.parseInt(deliveryTime.substring(0, 2)) * 60 + Integer.parseInt(deliveryTime.substring(3));
+        if(deliveryTime.length() != 0){
+            String hrs = deliveryTime.substring(0, 2);
+            String mnts = deliveryTime.substring(3);
+            this.deliveryTime = Integer.parseInt(hrs)*60 + Integer.parseInt(mnts);
+        }
+        else{
+            this.deliveryTime = 0;
+        }
     }
 
     public String getId() {
         return id;
     }
 
-    public int getDeliveryTime() {return deliveryTime;}
+    public int getDeliveryTime() {
+        return deliveryTime;
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
